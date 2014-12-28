36b32ac06b318bf1033cf6ff932615a9fd77a973
jdbellamy <jdbellamy@me.com>
2014-12-27 22:48:29 -0500

    testing heroku

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var fixtureData = require('../public/javascripts/fixture_data.json');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Dashboard', fixtureData: fixtureData });
+});
+
+
+module.exports = router;
