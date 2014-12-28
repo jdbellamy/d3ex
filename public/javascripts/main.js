e0ae500c59569949238fe13eb2a9b70c9f630c24
jdbellamy <jdbellamy@me.com>
2014-12-28 00:45:40 -0500

    second test

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,38 @@
-var url = '/javascripts/fixture_data.json';
 
-d3.json(url, function(error, data) {
+d3.json('/javascripts/fixture_data.json', function(error, data) {
 
-    console.log("data: " + JSON.stringify(data));
+  var x = d3.scale.linear().domain([0,d3.max(data)]).range([0,1000]);
 
-    var x = d3.scale.linear().domain([0,d3.max(data)]).range([0,1000]);
+  var r = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([200,0]),
+      g = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([0,0]),
+      b = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([100,0]);
 
-    var r = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([200,0]),
-        g = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([0,0]),
-        b = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([100,0]);
+  var c = function(x) { return d3.rgb(r(x),g(x),b(x)); }
 
-    var c = function(x) { return d3.rgb(r(x),g(x),b(x)); }
+  d3.select(".hbar")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .style("background-color", function(d) { return c(d); })
+    .text(function(d) { return d; });
+});
 
-    d3.select(".hbar")
-      .selectAll("div")
-      .data(data)
-      .enter().append("div")
-      .style("width", function(d) { return x(d) + "px"; })
-      .style("background-color", function(d) { return c(d); })
-      .style("text-align", "right")
-      .text(function(d) { return d; });
+d3.json('/javascripts/fixture_data.json', function(error, data) {
+
+  var x = d3.scale.linear().domain([0,d3.max(data)]).range([0,1000]);
+
+  var r = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([100,155]),
+      g = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([100,255]),
+      b = d3.scale.linear().domain([0,d3.max(data)]).rangeRound([255,0]);
+
+  var c = function(x) { return d3.rgb(r(x),g(x),b(x)); }
+
+  d3.select(".hbar2")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .style("background-color", function(d) { return c(d); })
+    .text(function(d) { return d; });
 });
